c5b1f39a4caa76bfd45ede76436b02e743b18ec3
dbushey <polendeuva@gmail.com>
2018-06-11 16:36:33 -0400

    minimum deliverables

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+  const form = document.getElementById('create-task-form');
+
+  // Adds a listener for the "submit" event.
+  form.addEventListener('submit', function(e) {
+    const task = document.getElementById("new-task-description").value;
+    e.preventDefault();
+    //console.log(task);
+
+    const tasksArea = document.getElementById("tasks");
+   // const taskLine = document.createElement('li');
+
+    tasksArea.innerHTML += `<li>${task}</li>`
+  });
+
+
+
+
+
 });
